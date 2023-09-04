66f31f73211645e02d81b385a025199410978939
imranovazer <57639829+imranovazer@users.noreply.github.com>
2023-09-04 15:07:05 +0400

    folder structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
 import { useState } from "react";
-
+import { AiOutlineInstagram } from "react-icons/ai";
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <div className="container mx-auto  px-6">
-      <h1 className="text-3xl font-bold underline text-red-500">
-        Hello Instagram
+      <h1 className="text-3xl font-bold flex gap-3 items-center  text-red-500">
+        <span>Hello Instagram</span>
+        <AiOutlineInstagram />
       </h1>
     </div>
   );
 }
 
 export default App;
-  
