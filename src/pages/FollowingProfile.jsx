15cdae018a78a2b63028300c2a55183d04765f16
Mirjabbar Badalov <mirishbedelov08@gmail.com>
2023-09-08 13:29:07 +0400

    Layout and routes changed

---
+++ src/pages/FollowingProfile.jsx
@@ -0,0 +1,39 @@
+import UserProfileDetails from "../components/UserProfileDetails";
+import AllPosts from "../components/AllPosts";
+import { useParams } from "react-router-dom";
+import { useSelector } from "react-redux";
+
+function FollowingProfile() {
+  const userData = useSelector((state) => state.userData.data);
+  const homeFeed = useSelector((state) => state?.homeFeed?.data);
+  const { username } = useParams();
+
+  const usernameToFind = username;
+
+  const subscriptionInfo = userData?.subscriptions?.find(
+    (subscription) => subscription.username === usernameToFind
+  );
+
+  const followingUserPosts = homeFeed?.filter(
+    (post) => post.authorUsername === username
+  );
+  // console.log(followingUserPosts);
+
+  return (
+    <div>
+      <UserProfileDetails
+        isFollowingPage={true}
+        firstName={subscriptionInfo?.firstName}
+        lastName={subscriptionInfo?.lastName}
+        userName={subscriptionInfo?.username}
+        postsLength={followingUserPosts?.length}
+      />
+      <AllPosts
+        isFollowingPage={true}
+        followingUserPosts={followingUserPosts}
+      />
+    </div>
+  );
+}
+
+export default FollowingProfile;
---
+++ src/routes/index.jsx
@@ -0,0 +1,35 @@
+import HomePage from "../pages/Home";
+import Login from "../pages/Login/Login";
+import ProfilePage from "../pages/Profile";
+import Register from "../pages/Register/Register";
+import FollowingProfile from "../pages/FollowingProfile";
+
+export const publicRoutes = [
+  {
+    key: 1,
+    path: "/login",
+    element: <Login />,
+  },
+  {
+    key: 2,
+    path: "/register",
+    element: <Register />,
+  },
+];
+export const protectedRoutes = [
+  {
+    key: 1,
+    path: "/",
+    element: <HomePage />,
+  },
+  {
+    key: 2,
+    path: "/profile",
+    element: <ProfilePage />,
+  },
+  {
+    key: 3,
+    path: "/following/:username",
+    element: <FollowingProfile />,
+  },
+];
