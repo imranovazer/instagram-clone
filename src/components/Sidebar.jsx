e6ef5b5da16d05232c11cf0ebf2aede59e6b4ade
Mirjabbar Badalov <mirishbedelov08@gmail.com>
2023-09-11 12:48:50 +0400

    Sidebar updated

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,31 +1,25 @@
 import { useSelector } from "react-redux";
 import UserInfo from "./UserInfo";
-
-function Sidebar() {
-  const userData = useSelector((state) => state?.userData?.data);
+function Sidebar({ isMyProfile }) {
   const userName = useSelector((state) => state?.userData?.data?.username);
   const userSubscriptions = useSelector(
     (state) => state?.userData?.data?.subscriptions
   );
-
   return (
     <div className="fixed right-[280px] mt-1 p-2">
-      <UserInfo userName={userName} isYourProfile={true} />
-      <div className="w-[300px] h-[300px]  overflow-y-auto mt-4 border-2 border-gray-200 rounded-md p-3">
+      <UserInfo
+        userName={userName}
+        isYourProfile={true}
+        isMyProfile={isMyProfile}
+      />
+      <div className="w-[300px] h-[300px]  mt-4 border-2 border-gray-200 rounded-md p-3">
         <div className="mb-2">Following</div>
         {userSubscriptions &&
           userSubscriptions.map((user, index) => (
             <UserInfo key={index} userName={user.username} />
           ))}
       </div>
-      {/* <div className="w-[300px] mt-4 border-2 border-gray-200 rounded-md p-3">
-        <div className="mb-2">Suggestions</div>
-        <UserInfo />
-        <UserInfo />
-        <UserInfo />
-      </div> */}
     </div>
   );
 }
-
 export default Sidebar;
